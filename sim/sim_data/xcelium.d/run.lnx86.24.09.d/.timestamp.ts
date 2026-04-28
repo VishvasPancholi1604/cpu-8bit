3f8aa8c9cf40edd2ff045ef9a96b31707ff1737b
VishvasPancholi1604 <vishvaspancholi.ec@gmail.com>
2026-04-28 07:50:35 -0400

    Remove sim_data from tracking and add to .gitignore

--- sim/sim_data/xcelium.d/run.lnx86.24.09.d/.timestamp.ts
+++
@@ -1,1 +0,0 @@
-1777376111 /u/pancholv/Desktop/sv_uvm/cpu/rtl/cpu.sv
--- sim/sim_data/xcelium.d/run.lnx86.24.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1727027473 /hwnet/cadence/xcelium/xcelium2024.09_s004/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1727027472 /hwnet/cadence/xcelium/xcelium2024.09_s004/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
